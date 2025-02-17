945793252dec1f6f4854ffd1d3b84dd6d661eb84
zaakiraza <raaza.zaakir@gmail.com>
2025-02-17 23:48:33 +0500

    some updates

--- firebase.js
+++ firebase.js
@@ -93,3 +93,14 @@ export async function logout() {
         window.location.href = "../login/login.html";
     } catch (error) { }
 }
+
+export async function documentShowingOnPlaceholder(uid) {
+    const docRef = doc(db, "users", uid);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        return docSnap.data();
+    } else {
+        return("No such document!");
+    }
+}
--- home/home.js
+++ home/home.js
@@ -1,13 +1,41 @@
-import { editDetails, getSingleData, stateObserver,logout } from "../firebase.js";
+import { editDetails, getSingleData, stateObserver, logout, documentShowingOnPlaceholder } from "../firebase.js";
 
 // Get modal and buttons
 const editProfileBtn = document.getElementById("editProfileBtn");
 const modal = document.getElementById("editProfileUpModal");
 const closeModalBtn = document.getElementsByClassName("close-btn")[0];
 
+const homelogoutBtn = document.getElementById('homelogoutBtn');
+const editBtn = document.getElementById('editBtn');
+let imgUserFeild = document.getElementById('img');
+let nameUserFeild = document.getElementById('name');
+let desUserFeild = document.getElementById('des');
+let emailUserFeild = document.getElementById('email');
+let editName = document.getElementById('editName');
+let editDes = document.getElementById('editDes');
+let editImg = document.getElementById('editImg');
+let editEmail = document.getElementById('editEmail');
+
+async function starter() {
+    uid = await stateObserver();
+    userDetails = await getSingleData(uid);
+    let { name, email, description, imgURL } = userDetails;
+    nameUserFeild.innerText = name.toUpperCase();
+    desUserFeild.innerText = description || "No Description";
+    imgUserFeild.src = imgURL || "https://static.vecteezy.com/system/resources/thumbnails/003/337/584/small/default-avatar-photo-placeholder-profile-icon-vector.jpg";
+    emailUserFeild.innerText = email;
+}
+starter();
+
 // Open Modal
-editProfileBtn.onclick = function () {
+editProfileBtn.onclick = async function () {
     modal.style.display = "block";
+    let data = await documentShowingOnPlaceholder(uid);
+    let { description, email, imgURL, name } = data;
+    editName.value = name;
+    editEmail.value = email;
+    editDes.value = description;
+    editImg.value = imgURL;
 }
 
 // Close Modal when clicking on "X"
@@ -22,32 +50,10 @@ window.onclick = function (event) {
     }
 }
 
-const homelogoutBtn = document.getElementById('homelogoutBtn');
-const editBtn = document.getElementById('editBtn');
-let imgUserFeild=document.getElementById('img');
-let nameUserFeild=document.getElementById('name');
-let desUserFeild=document.getElementById('des');
-let emailUserFeild=document.getElementById('email');
-let editName=document.getElementById('editName');
-let editDes=document.getElementById('editDes');
-let editImg=document.getElementById('editImg');
-let editEmail=document.getElementById('editEmail');
-
 let uid;
 let userDetails;
 
-async function starter() {   
-    uid =await stateObserver();
-    userDetails=await getSingleData(uid);   
-    let {name ,email,description,imgURL} = userDetails;
-    nameUserFeild.innerText=name.toUpperCase();
-    desUserFeild.innerText=description || "No Description";
-    imgUserFeild.src=imgURL || "https://static.vecteezy.com/system/resources/thumbnails/003/337/584/small/default-avatar-photo-placeholder-profile-icon-vector.jpg";
-    emailUserFeild.innerText=email;
-}
-starter();
-
-editBtn.onclick = function (e){
+editBtn.onclick = function (e) {
     e.preventDefault();
     console.log(emailUserFeild.value)
     editDetails({
@@ -55,10 +61,10 @@ editBtn.onclick = function (e){
         email: editEmail.value,
         imgURL: editImg.value || "https://static.vecteezy.com/system/resources/thumbnails/003/337/584/small/default-avatar-photo-placeholder-profile-icon-vector.jpg",
         description: editDes.value || ""
-    },uid);
+    }, uid);
 }
 
-homelogoutBtn.onclick = function (e){
+homelogoutBtn.onclick = function (e) {
     e.preventDefault();
     logout();
 }
